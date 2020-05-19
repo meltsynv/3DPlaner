138bccdce3f344a91af81dc479e9a255614efaad
Lisa Wolff <lisa.wolff@stud.hs-ruhrwest.de>
2020-05-19 15:39:54 +0200

    Screenshot Ansätze

---
+++ 3D_Planer_Unity/Assets/Scripts/Screenshot.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class Screenshot : MonoBehaviour
+{
+    
+    // GameObject blink; --> Für Blitz geht noch nicht
+    
+    public void TakeAShot()
+    {
+        StartCoroutine (nameof(CaptureIt));
+    }
+
+    private IEnumerator CaptureIt()
+    {
+        
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "Screenshot" + timeStamp + ".png";
+        string pathToSave = fileName;
+        ScreenCapture.CaptureScreenshot(pathToSave);
+        yield return new WaitForEndOfFrame();
+        
+        //Instantiate (blink, new Vector2(0f, 0f), Quaternion.identity); --> Blitz s.o.
+        
+    }
+}
